4419e15bf16519872dd5ce00e6e0c654c308bdb8
Pavan <pavankramadugu@gmail.com>
2021-04-10 15:36:15 +0530

    error handling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import { forwardRef } from 'react';
+import {forwardRef} from 'react';
 import Avatar from 'react-avatar';
 import Grid from '@material-ui/core/Grid'
+import validator from 'validator';
 
 import MaterialTable from "material-table";
 import AddBox from '@material-ui/icons/AddBox';
@@ -24,208 +25,212 @@ import axios from 'axios'
 import Alert from '@material-ui/lab/Alert';
 
 const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref}/>),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref}/>),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref}/>),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref}/>),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref}/>),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>)
 };
 
 const api = axios.create({
-  baseURL: `https://student-crud-cogoport.herokuapp.com/api`
+    baseURL: `https://student-crud-cogoport.herokuapp.com/api`
 })
 
 
-function validateEmail(email){
-  const re = /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/;
-  return re.test(String(email).toLowerCase());
+function validateEmail(email) {
+   return validator.isEmail('foo@bar.com')
+}
+
+function validatePhone(phone) {
+    return validator.isMobilePhone(phone);
 }
 
 function App() {
 
-  var columns = [
-    {title: "id", field: "id", hidden: true},
-    {title: "First name", field: "firstName"},
-    {title: "Last name", field: "lastName"},
-    {title: "College", field: "college"},
-    {title: "Email", field: "emailId"},
-    {title: "Phone", field: "phone"}
-  ]
-  const [data, setData] = useState([]); //table data
+    var columns = [
+        {title: "id", field: "id", hidden: true},
+        {title: "First name", field: "firstName"},
+        {title: "Last name", field: "lastName"},
+        {title: "College", field: "college"},
+        {title: "Email", field: "emailId"},
+        {title: "Phone", field: "phone"}
+    ]
+    const [data, setData] = useState([]); //table data
 
-  //for error handling
-  const [iserror, setIserror] = useState(false)
-  const [errorMessages, setErrorMessages] = useState([])
+    //for error handling
+    const [iserror, setIserror] = useState(false)
+    const [errorMessages, setErrorMessages] = useState([])
 
-  useEffect(() => {
-    api.get("/getAll")
-        .then(res => {
-            setData(res.data)
-         })
-         .catch(error=>{
-             console.log("Error")
-         })
-  }, [])
+    useEffect(() => {
+        api.get("/getAll")
+            .then(res => {
+                setData(res.data)
+            })
+            .catch(error => {
+                console.log("Error")
+            })
+    }, [])
 
-  const handleRowUpdate = (newData, oldData, resolve) => {
-    //validation
-    let errorList = []
-    if(newData.first_name === ""){
-      errorList.push("Please enter first name")
-    }
-    if(newData.last_name === ""){
-      errorList.push("Please enter last name")
-    }
-    if(newData.email === ""){
-      errorList.push("Please enter a valid email")
-    }
-    if(newData.college === ""){
-          errorList.push("Please enter a valid College Name")
+    const handleRowUpdate = (newData, oldData, resolve) => {
+        //validation
+        let errorList = []
+        if (newData.firstName === undefined) {
+            errorList.push("Please enter first name")
         }
-    if(newData.phone === ""){
-          errorList.push("Please enter a valid Phone Number")
-    }
-
-    if(errorList.length < 1){
-      api.put("/updateUser", newData)
-      .then(res => {
-        const dataUpdate = [...data];
-        const index = oldData.tableData.id;
-        dataUpdate[index] = newData;
-        setData([...dataUpdate]);
-        resolve()
-        setIserror(false)
-        setErrorMessages([])
-      })
-      .catch(error => {
-        setErrorMessages(["Update failed! Server error, Please Retry"])
-        setIserror(true)
-        resolve()
-
-      })
-    }else{
-      setErrorMessages(errorList)
-      setIserror(true)
-      resolve()
-
-    }
-
-  }
-
-  const handleRowAdd = (newData, resolve) => {
-    //validation
-    let errorList = []
-    if(newData.first_name === ""){
-      errorList.push("Please enter first name")
-    }
-    if(newData.last_name === ""){
-      errorList.push("Please enter last name")
-    }
-    if(newData.email === ""){
-          errorList.push("Please enter a valid email")
+        if (newData.lastName === undefined) {
+            errorList.push("Please enter last name")
         }
-        if(newData.college === ""){
-              errorList.push("Please enter a valid College Name")
-            }
-        if(newData.phone === ""){
-              errorList.push("Please enter a valid Phone Number")
+        if (newData.emailId === undefined || validateEmail(newData.email) === false) {
+            errorList.push("Please enter a valid email")
+        }
+        if (newData.college === undefined) {
+            errorList.push("Please enter a valid College Name")
+        }
+        if (newData.phone === undefined || validatePhone(newData.phone) === false) {
+            errorList.push("Please enter a valid Phone Number")
         }
 
-    if(errorList.length < 1){ //no error
-      api.post("/addUser", newData)
-      .then(res => {
-        let dataToAdd = [...data];
-        dataToAdd.push(newData);
-        setData(dataToAdd);
-        resolve()
-        setErrorMessages([])
-        setIserror(false)
-      })
-      .catch(error => {
-        setErrorMessages(["Cannot add data. Server error!, Please Retry"])
-        setIserror(true)
-        resolve()
-      })
-    }else{
-      setErrorMessages(errorList)
-      setIserror(true)
-      resolve()
+        if (errorList.length < 1) {
+            api.put("/updateUser", newData)
+                .then(res => {
+                    const dataUpdate = [...data];
+                    const index = oldData.tableData.id;
+                    dataUpdate[index] = newData;
+                    setData([...dataUpdate]);
+                    resolve()
+                    setIserror(false)
+                    setErrorMessages([])
+                })
+                .catch(error => {
+                    setErrorMessages(["Update failed! Server error, Please Retry"])
+                    setIserror(true)
+                    resolve()
+
+                })
+        } else {
+            setErrorMessages(errorList)
+            setIserror(true)
+            resolve()
+
+        }
+
+    }
+
+    const handleRowAdd = (newData, resolve) => {
+        //validation
+        let errorList = []
+        if (newData.firstName === undefined) {
+            errorList.push("Please enter first name")
+        }
+        if (newData.lastName === undefined) {
+            errorList.push("Please enter last name")
+        }
+        if (newData.emailId === undefined || validateEmail(newData.email) === false) {
+            errorList.push("Please enter a valid email")
+        }
+        if (newData.college === undefined) {
+            errorList.push("Please enter a valid College Name")
+        }
+        if (newData.phone === undefined || validatePhone(newData.phone) === false) {
+            errorList.push("Please enter a valid Phone Number")
+        }
+
+        if (errorList.length < 1) { //no error
+            api.post("/addUser", newData)
+                .then(res => {
+                    let dataToAdd = [...data];
+                    dataToAdd.push(newData);
+                    setData(dataToAdd);
+                    resolve()
+                    setErrorMessages([])
+                    setIserror(false)
+                })
+                .catch(error => {
+                    setErrorMessages(["Cannot add data. Server error!, Please Retry by Refreshing"])
+                    setIserror(true)
+                    resolve()
+                })
+        } else {
+            setErrorMessages(errorList)
+            setIserror(true)
+            resolve()
+        }
+
+
+    }
+
+    const handleRowDelete = (oldData, resolve) => {
+
+        api.delete("/deleteUser/" + oldData.id)
+            .then(res => {
+                const dataDelete = [...data];
+                const index = oldData.tableData.id;
+                dataDelete.splice(index, 1);
+                setData([...dataDelete]);
+                resolve()
+            })
+            .catch(error => {
+                console.log(error)
+                setErrorMessages(["Delete failed! Server error, Please Retry by Refreshing"])
+                setIserror(true)
+                resolve()
+            })
     }
 
 
-  }
+    return (
+        <div className="App">
 
-  const handleRowDelete = (oldData, resolve) => {
+            <Grid container spacing={1}>
+                <Grid item xs={3}></Grid>
+                <Grid item xs={6}>
+                    <div>
+                        {iserror &&
+                        <Alert severity="error">
+                            {errorMessages.map((msg, i) => {
+                                return <div key={i}>{msg}</div>
+                            })}
+                        </Alert>
+                        }
+                    </div>
+                    <MaterialTable
+                        title="Student Details"
+                        columns={columns}
+                        data={data}
+                        icons={tableIcons}
+                        editable={{
+                            onRowUpdate: (newData, oldData) =>
+                                new Promise((resolve) => {
+                                    handleRowUpdate(newData, oldData, resolve);
 
-    api.delete("/deleteUser/"+oldData.id)
-      .then(res => {
-        const dataDelete = [...data];
-        const index = oldData.tableData.id;
-        dataDelete.splice(index, 1);
-        setData([...dataDelete]);
-        resolve()
-      })
-      .catch(error => {
-        setErrorMessages(["Delete failed! Server error, Please Retry"])
-        setIserror(true)
-        resolve()
-      })
-  }
-
-
-  return (
-    <div className="App">
-
-      <Grid container spacing={1}>
-          <Grid item xs={3}></Grid>
-          <Grid item xs={6}>
-          <div>
-            {iserror &&
-              <Alert severity="error">
-                  {errorMessages.map((msg, i) => {
-                      return <div key={i}>{msg}</div>
-                  })}
-              </Alert>
-            }
-          </div>
-            <MaterialTable
-              title="Student Details"
-              columns={columns}
-              data={data}
-              icons={tableIcons}
-              editable={{
-                onRowUpdate: (newData, oldData) =>
-                  new Promise((resolve) => {
-                      handleRowUpdate(newData, oldData, resolve);
-
-                  }),
-                onRowAdd: (newData) =>
-                  new Promise((resolve) => {
-                    handleRowAdd(newData, resolve)
-                  }),
-                onRowDelete: (oldData) =>
-                  new Promise((resolve) => {
-                    handleRowDelete(oldData, resolve)
-                  }),
-              }}
-            />
-          </Grid>
-          <Grid item xs={3}></Grid>
-        </Grid>
-    </div>
-  );
+                                }),
+                            onRowAdd: (newData) =>
+                                new Promise((resolve) => {
+                                    handleRowAdd(newData, resolve)
+                                }),
+                            onRowDelete: (oldData) =>
+                                new Promise((resolve) => {
+                                    handleRowDelete(oldData, resolve)
+                                }),
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={3}></Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
